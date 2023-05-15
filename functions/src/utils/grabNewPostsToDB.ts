46e7dddb90b7ec5199267c99c47922d21ba52029
Antonnfs <anton.nfs1@gmail.com>
2023-05-15 18:27:52 +0300

    Improve grabNewPosts logic

--- functions/src/utils/grabNewPostsToDB.ts
+++ functions/src/utils/grabNewPostsToDB.ts
@@ -1,19 +1,19 @@
 import * as Parser from "rss-parser";
+import * as jsdom from "jsdom";
 import { FeedItem, FeedProps } from "../types/feed";
 import { COLLECTIONS } from "../constants";
 import { db } from "..";
 
 export const grabNewPostsToDB = async () => {
    const parser: Parser<FeedProps, FeedItem> = new Parser();
-   const domParser = new DOMParser();
    const postsSnap = db.collection(COLLECTIONS.POSTS);
    const feed = await parser.parseURL("https://lifehacker.com/rss");
    const docsSnap = await postsSnap.get();
    const docsIds = docsSnap.docs.map((doc) => doc.id);
    feed.items.forEach((item) => {
       if (!docsIds.includes(item.guid)) {
-         const htmlDoc = domParser.parseFromString(item.content, "text/xml");
-         const image = htmlDoc.getElementsByTagName("img").item(0)?.src;
+         const dom = new jsdom.JSDOM(`<!DOCTYPE html>${item.content}`);
+         const image = dom.window.document.querySelector("img")?.src;
          postsSnap.doc(item.guid).set({ ...item, image });
       }
    });
