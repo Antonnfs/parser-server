42ac4d3fc84194a5a2b6ccdff710661291e75797
Antonnfs <anton.nfs1@gmail.com>
2023-05-17 11:47:08 +0300

    Pagination error handlings

--- functions/src/routes/index.ts
+++ functions/src/routes/index.ts
@@ -1,65 +1,26 @@
 import { Request, Response, Router as getRouter } from "express";
 import { db } from "..";
-// import { COLLECTIONS } from "../constants";
+import { getPaginatedData } from "../utils/pagination";
 
 const router = getRouter();
 
-export interface PaginationProps {
-   ref: FirebaseFirestore.CollectionReference<FirebaseFirestore.DocumentData>;
-   limit: number;
-   last?: string;
-   first?: string;
-}
-
-const getInitPage = async ({ ref, limit }: PaginationProps) => {
-   return await ref.orderBy("isoDate", "desc").limit(limit).get();
-};
-
-const getNextPage = async ({ ref, last, limit }: PaginationProps) => {
-   return await ref
-      .orderBy("isoDate", "desc")
-      .startAfter(last)
-      .limit(limit)
-      .get();
-};
-
-const getPrevPage = async ({ ref, first, limit }: PaginationProps) => {
-   return await ref
-      .orderBy("isoDate", "desc")
-      .endBefore(first)
-      .limitToLast(limit)
-      .get();
-};
-
-const getPage = async ({ ref, limit, first, last }: PaginationProps) => {
-   if (last) return await getNextPage({ ref, last, limit });
-   if (first) return await getPrevPage({ ref, first, limit });
-   return await getInitPage({ ref, limit });
-};
-
-export const getPaginatedData = async ({
-   ref,
-   last,
-   first,
-   limit,
-}: PaginationProps) => {
-   const pageSnapshot = await getPage({ ref, limit, first, last });
-   const items = await pageSnapshot.docs.map((item) => item.data());
-   const count = (await ref.count().get()).data().count;
-	
-   // const pages = count / 10;
-   return { items, count };
-};
-
 export const getPosts = async (req: Request, res: Response) => {
-   const last = typeof req.query.last === "string" ? req.query.last : undefined;
-   const first =
-      typeof req.query.first === "string" ? req.query.first : undefined;
-   const limit = typeof req.query.limit === "string" ? +req.query.limit : 10;
-   console.log(req.route.path);
-   const ref = db.collection(req.route.path);
-   const data = await getPaginatedData({ ref, last, first, limit });
-   res.json(data);
+   try {
+      const last =
+         typeof req.query.last === "string" ? req.query.last : undefined;
+      const first =
+         typeof req.query.first === "string" ? req.query.first : undefined;
+      const limit =
+         typeof req.query.limit === "string" ? parseInt(req.query.limit) : 10;
+      const page =
+         typeof req.query.page === "string" ? parseInt(req.query.page) : 1;
+      console.log(req.route.path);
+      const ref = db.collection(req.route.path);
+      const data = await getPaginatedData({ ref, last, first, limit, page });
+      res.json(data);
+   } catch (error) {
+      res.status(400).json(error);
+   }
 };
 
 router.get("/posts", getPosts);
---
+++ functions/src/utils/pagination.ts
@@ -0,0 +1,76 @@
+export interface PaginationProps {
+   ref: FirebaseFirestore.CollectionReference<FirebaseFirestore.DocumentData>;
+   limit: number;
+   last?: string;
+   first?: string;
+   page?: number;
+}
+
+const getInitPage = async ({ ref, limit }: PaginationProps) => {
+   try {
+      return await ref.orderBy("isoDate", "desc").limit(limit).get();
+   } catch (error) {
+      throw error;
+   }
+};
+
+const getNextPage = async ({ ref, last, limit }: PaginationProps) => {
+   try {
+      return await ref
+         .orderBy("isoDate", "desc")
+         .startAfter(last)
+         .limit(limit)
+         .get();
+   } catch (error) {
+      throw error;
+   }
+};
+
+const getPrevPage = async ({ ref, first, limit }: PaginationProps) => {
+   try {
+      return await ref
+         .orderBy("isoDate", "desc")
+         .endBefore(first)
+         .limitToLast(limit)
+         .get();
+   } catch (error) {
+      throw error;
+   }
+};
+
+const getPage = async ({
+   ref,
+   limit,
+   first,
+   last,
+}: PaginationProps): Promise<
+   FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData>
+> => {
+   try {
+      if (last) return await getNextPage({ ref, last, limit });
+      if (first) return await getPrevPage({ ref, first, limit });
+      return await getInitPage({ ref, limit });
+   } catch (error) {
+      throw error;
+   }
+};
+
+export const getPaginatedData = async ({
+   ref,
+   last,
+   first,
+   limit,
+   page,
+}: PaginationProps) => {
+   try {
+      const count = (await ref.count().get()).data().count;
+      const pages = Math.ceil(count / limit);
+      if (!page || page < 1) page = 1;
+      if (page > count) page = count;
+      const pageSnapshot = await getPage({ ref, limit, first, last });
+      const items = await pageSnapshot.docs.map((item) => item.data());
+      return { items, count, page, pages };
+   } catch (error) {
+      throw error;
+   }
+};
